f06e991c16f7d6e75263d228276b040f3989eb02
sebastiangp2004 <sebastiangp20044@gmail.com>
2025-10-30 11:46:50 -0500

    laravel

---
+++ 20-laravel/resources/views/view-pets.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>List All Pets</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body class="bg-teal-900 p-10 min-h-[100dvh]">
+    <h1 class="text-white text-center text-4xl border-b-2 pb-4">List All Pets 🐣</h1>
+    <section class="p-10 flex gap-4 flex-wrap justify-center">
+        @foreach($pets as $pet)
+        <div class="card card-side bg-base-100 shadow-sm">
+            <figure class="w-[140px]">
+                <img src="{{asset('images/'.$pet->image)}}" alt="{{$pet->name}}" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">{{$pet->name}}</h2>
+                @if($pet->kind == 'Dog')
+                <div class="badge badge-primary">Dog</div>
+                @elseif($pet->kind == 'Cat')
+                <div class="badge badge-success">Cat</div>
+                @elseif($pet->kind == 'Pig')
+                <div class="badge badge-secondary">Pig</div>
+                @elseif($pet->kind == 'Bird')
+                <div class="badge badge-warning">Bird</div>
+                @endif
+                <p class="w-[200px]">{{$pet->description}}</p>
+                <div class="card-actions justify-end">
+                    <button class="btn text-white bg-teal-700 rounded-full p-1 hover:scale-150 transition-all ">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#000000" viewBox="0 0 256 256"><path d="M152,112a8,8,0,0,1-8,8H120v24a8,8,0,0,1-16,0V120H80a8,8,0,0,1,0-16h24V80a8,8,0,0,1,16,0v24h24A8,8,0,0,1,152,112Zm77.66,117.66a8,8,0,0,1-11.32,0l-50.06-50.07a88.11,88.11,0,1,1,11.31-11.31l50.07,50.06A8,8,0,0,1,229.66,229.66ZM112,184a72,72,0,1,0-72-72A72.08,72.08,0,0,0,112,184Z"></path></svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </section>
+</body>
+</html>
--- 20-laravel/routes/web.php
+++ 20-laravel/routes/web.php
@@ -3,5 +3,62 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    // return "This is a entry point  ";
     return view('welcome');
+});
+
+
+Route::get('hello', function () {
+    return "<h1>Hello folks, Have a nice day!</h1>";
+});
+
+Route::get('hello/{name}', function () {
+    return "<h1>Hello ". request()->name .", Have a nice day!</h1>";
+});
+
+Route::get('show/pets', function () {
+    $pets = \App\Models\Pet::all();
+    dd($pets->toArray()); // dump and die
+});
+Route::get('show/pets/{id}', function ($id) {
+    $pet = \App\Models\Pet::find($id);
+    dd($pet->toArray()); // buscar por id
+});
+
+Route::get('show/users', function () {
+    $users = \App\Models\User::all();
+    dd($users->toArray());
+});
+
+
+Route::get('challenge', function () {
+    $users = \App\Models\User::take(20)->get();
+    
+    $html = '<table border="1" style="border-collapse: collapse; width: 50%; justify-content: center; margin: auto;">
+        <tr style="background-color: #f2f2f2;">
+            <th>ID</th>
+            <th>Photo</th>
+            <th>Full Name</th>
+            <th>Age</th>
+            <th>Created</th>
+        </tr>';
+    
+    foreach ($users as $user) {
+        $html .= '<tr>
+            <td style="text-align: center;">' . $user->id . '</td>
+            <td style="text-align: center;"><img src="' . $user->photo . '" width="100"></td>
+            <td style="padding: 10px;">' . $user->fullname . '</td>
+            <td style="text-align: center;">' . \Carbon\Carbon::parse($user->birthdate)->age . '</td>
+            <td style="padding: 10px;">' . $user->created_at->diffForHumans() . '</td>
+        </tr>';
+    }
+    
+    $html .= '</table>';
+    
+    return $html;
+});
+
+Route::get('view/pets', function() {
+    $pets = App\Models\Pet::all();
+    return view('view-pets')->with('pets', $pets);
 });
